6d49cee094f828b8a2d8d72d266e030c459c5d1f
walter <hietanen@gmail.com>
2019-06-06 01:17:44 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert("wtf? "+ this.state.value);
+    event.preventDefault();
+  }
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +37,23 @@ function App() {
         </a>
       </header>
     </div>
+  );*/
+
+render() {
+  return (
+    <div>
+      <h1>Da Guestbook</h1>
+      <div className="card">
+        <h3>Enter your shit</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.value} onChange={this.handleChange} type="text" />
+          <input className="button" type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
   );
+}
+  
 }
 
 export default App;
---
+++ src/submission.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class submission extends React.Component {
+
+    constructor(props) {
+
+    }
+}
